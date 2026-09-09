ec2ac94ac32bdc34fe1b1c8964bbd40edf564304
vtu27594-bit <vtu27594@veltech.edu.in>
2026-09-09 09:14:56 +0530

    Time: 31 ms (83.56%), Space: 99.3 MB (96.71%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.java
@@ -0,0 +1,34 @@
+class MinStack {
+
+    private Stack<Integer> stack;
+	private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+		minStack = new Stack<>();
+    }
+    
+    public void push(int value) {
+        stack.push(value);
+
+		if (minStack.isEmpty() || value <= minStack.peek()) {
+			minStack.push(value);
+		}
+    }
+    
+    public void pop() {
+        int value = stack.pop();
+
+		if (value == minStack.peek()) {
+			minStack.pop();
+		}
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
