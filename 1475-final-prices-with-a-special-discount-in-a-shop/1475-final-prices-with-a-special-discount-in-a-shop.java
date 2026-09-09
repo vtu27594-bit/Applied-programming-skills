e992c3b0328f4779c24aa0e7e83a6b625892d744
vtu27594-bit <vtu27594@veltech.edu.in>
2026-09-09 09:10:48 +0530

    Time: 3 ms (60.17%), Space: 45.2 MB (58.08%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        for(int i = 0; i < prices.length; i++){
+            while(!stack.isEmpty() && prices[stack.peek()]>=prices[i]){
+                prices[stack.pop()]-=prices[i];
+            }
+            stack.push(i);
+        }
+        return prices;
+    }
+}
